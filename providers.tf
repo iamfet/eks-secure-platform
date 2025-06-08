77da19321892ce20e976f493da0f01ae5ed896d4
iamfet <fetuyee@gmail.com>
2025-06-08 19:15:26 +0000

    infra(providers): add s3 backend

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket       = "eks-state-bucket-iac"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true                              # Enable S3 native state locking, remove if using DynamoDB for state locking
+    /*dynamodb_table = "terraform-eks-state-locks"*/ # Uncomment if using DynamoDB for state locking
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
