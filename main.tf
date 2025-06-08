c81d754d1d8d8e274d20e074378faca00600ad2f
iamfet <fetuyee@gmail.com>
2025-06-08 20:33:59 +0000

    refactor(infra): format code

--- main.tf
+++ main.tf
@@ -108,17 +108,17 @@ module "eks" {
     }
   }
 
-  tags = {
-    environment = "development"
-    application = "${var.project_name}"
-  }
-
   eks_managed_node_groups = {
     dev = {
       instance_types = ["t2.large"]
       min_size       = 1
       max_size       = 3
-      desired_size   = 3
+      desired_size   = 4
     }
   }
+  tags = {
+    environment = "development"
+    application = "${var.project_name}"
+  }
+
 }
